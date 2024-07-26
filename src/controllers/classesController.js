fcec39fdc33c6fa30326adc4da2ff6ab997f75b9
Mariya Doronkina <127694722+mariyador@users.noreply.github.com>
2024-07-26 01:58:53 -0400

    minor changes

--- src/controllers/classesController.js
+++ src/controllers/classesController.js
@@ -4,7 +4,7 @@ const getAllClasses = async (req, res) => {
   try {
     const classes = await Class.find();
     console.log("Classes retrieved:", classes);
-    res.status(200).json(classes);
+    res.status(201).json(classes);
   } catch (error) {
     console.error("Error retrieving classes:", error);
     res.status(500).json({ message: "Internal server error" });
