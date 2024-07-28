6f5770ff9be6441b6cc5dabd65f6624ad8ee6869
mariyador <aniknorod@gmail.com>
2024-07-27 22:52:07 -0400

    add middleware for authentication, error handling, and 404 responses to app.js

--- src/app.js
+++ src/app.js
@@ -1,30 +1,37 @@
-require('dotenv').config({ path: __dirname + '/../.env' });
+require("dotenv").config({ path: __dirname + "/../.env" });
 
-const express = require('express');
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
-const favicon = require('express-favicon');
-const logger = require('morgan');
+const cors = require("cors");
+const favicon = require("express-favicon");
+const logger = require("morgan");
 
-const mainRouter = require('./routes/mainRouter.js');
-const userRouter = require('./routes/userRouter.js')
-const classesRouter = require('./routes/classesRouter.js')
+const authenticationMiddleware = require("./middleware/authentication.js");
+const errorHandlerMiddleware = require("./middleware/error-handler.js");
+const notFound = require("./middleware/notFound.js");
 
+const mainRouter = require("./routes/mainRouter.js");
+const userRouter = require("./routes/userRouter.js");
+const classesRouter = require("./routes/classesRouter.js");
 
-const connectDB = require('./db/db.js');
+const connectDB = require("./db/db.js");
 connectDB();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger('dev'));
-app.use(express.static('public'))
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // routes
-app.use('/api/v1', mainRouter, userRouter , classesRouter);
-app.use('/api/v1/users', userRouter, classesRouter);
+app.use("/api/v1", mainRouter, userRouter, classesRouter);
+app.use("/api/v1/users", userRouter, classesRouter);
+
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
 module.exports = app;
