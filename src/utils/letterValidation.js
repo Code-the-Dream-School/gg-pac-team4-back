5ac7c168cac6668fc6a3ad702f83a36d48499b43
Oksana Fetter <fetter.oksana@gmail.com>
2024-08-06 20:44:12 -0400

    fix adultName validation

--- src/utils/letterValidation.js
+++ src/utils/letterValidation.js
@@ -1,6 +1,6 @@
 const lettersOnlyValidator = {
   validator: function (value) {
-    return /^[A-Za-z]+$/.test(value);
+    return /^[A-Za-z\s]+$/.test(value);
   },
   message: (props) => `${props.path} should contain only letters`,
 };
